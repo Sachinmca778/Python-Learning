9318f051c752320e334a1bbd69d0681fc08819f5
Sachin <sachin@Narendras-MacBook-Air.local>
2024-11-23 17:23:30 +0530

    python basic

---
+++ 01_basics/Basic.py
@@ -0,0 +1,17 @@
+#1.Run the program using the command:
+#python Basic.py           (In window)
+#python3 Basic.py          (In Mac)
+
+#2.Print statement
+print('my name is sachin')
+
+
+#3.Function definition
+def fruit(n):
+    print(n)
+
+#4.Function call
+fruit('apple')
+
+
+
---
+++ 01_basics/Basic2.py
@@ -0,0 +1,7 @@
+from Basic import fruit        #importing the function fruit from Basic.py file
+
+fruit('Banana')                #calling the function from Basic.py file
+
+#Output:
+#apple
+#Banana
